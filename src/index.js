28b3c2456693ed6bb60f282f2bc0aacebfcbebc7
linkFly <linkFly6@live.com>
2017-03-25 04:18:07 +0800

    细节 fix

--- src/index.js
+++ src/index.js
@@ -7,17 +7,11 @@ import { Router, Route, browserHistory } from 'react-router'
 import AddSaid from  './views/Said/AddSaid'
 import Index from  './views/Index/Index'
 
-class Test extends Component {
-  render() {
-    return <h3>About</h3>
-  }
-}
 ReactDOM.render(
   <Router history={browserHistory}>
     <Route path="/" component={App}>
       <Route path="index" component={Index} />
       <Route path="said/add" component={AddSaid} />
-      <Route path="test" component={Test} />
     </Route>
   </Router>,
   document.getElementById('root')
